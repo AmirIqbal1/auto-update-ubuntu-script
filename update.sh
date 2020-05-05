f2a708a987fc07bf07d919a6285d5b1796f64d10
Amir <AmirIqbal1@users.noreply.github.com>
2020-05-05 13:08:42 +0100

    removed -get
    removed -get to see the progress bar at the bottom of terminal

--- update.sh
+++ update.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 dpkg --configure -a
-apt-get update -y
-apt-get upgrade -y
-apt-get install -f
-apt-get clean
-apt-get autoclean
-apt-get autoremove -y
+apt update -y
+apt upgrade -y
+apt install -f
+apt clean
+apt autoclean
+apt autoremove -y
 
 echo "cleaning bash"
 rm .bash_history

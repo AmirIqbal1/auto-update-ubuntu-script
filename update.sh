49d316c54876ba7f8cd60173a419456ff698aaf9
Amir <AmirIqbal1@users.noreply.github.com>
2021-01-04 14:20:00 +0000

    streamlined 2.0

--- update.sh
+++ update.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
-dpkg --configure -a
-apt update -y
-apt upgrade -y
-apt install -f
-apt clean
-apt autoclean
+dpkg --configure -a &&
+apt update &&
+apt upgrade -y &&
+apt install -f &&
+apt clean &&
+apt autoclean &&
 apt autoremove -y
-
-echo "cleaning bash"
-rm .bash_history

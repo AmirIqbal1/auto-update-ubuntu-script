075d9c1a2e6ab3fe9fb559f83450366fad81cecf
Amir <AmirIqbal1@users.noreply.github.com>
2024-06-27 18:37:14 +0100

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,14 +1,70 @@
 #!/bin/bash
-dpkg --configure -a &&
-apt update &&
-apt upgrade -y &&
-#flatpak update -y &&
-apt install -f &&
-apt clean &&
-apt autoclean &&
-apt autoremove -y
 
-#remove bottom lines for auto reboot is needed 
-#if [ -f /var/run/reboot-required ]; then
-#  reboot
-#fi
+# Function to print messages
+print_msg() {
+  echo "$(date '+%Y-%m-%d %H:%M:%S') - $1"
+}
+
+# Start of the script
+print_msg "Starting system update..."
+
+# Reconfigure dpkg
+print_msg "Reconfiguring dpkg..."
+dpkg --configure -a
+if [ $? -ne 0 ]; then
+  print_msg "dpkg reconfiguration failed."
+  exit 1
+fi
+
+# Update package lists
+print_msg "Updating package lists..."
+apt update
+if [ $? -ne 0 ]; then
+  print_msg "apt update failed."
+  exit 1
+fi
+
+# Upgrade packages
+print_msg "Upgrading packages..."
+apt upgrade -y
+if [ $? -ne 0 ]; then
+  print_msg "apt upgrade failed."
+  exit 1
+fi
+
+# Uncomment the following lines to update Flatpak packages
+# print_msg "Updating Flatpak packages..."
+# flatpak update -y
+# if [ $? -ne 0 ]; then
+#   print_msg "Flatpak update failed."
+#   exit 1
+# fi
+
+# Fix broken dependencies
+print_msg "Fixing broken dependencies..."
+apt install -f
+if [ $? -ne 0 ]; then
+  print_msg "Fixing broken dependencies failed."
+  exit 1
+fi
+
+# Clean up
+print_msg "Cleaning up unnecessary packages and files..."
+apt clean
+apt autoclean
+apt autoremove -y
+if [ $? -ne 0 ]; then
+  print_msg "Clean up failed."
+  exit 1
+fi
+
+# Auto reboot if needed
+if [ -f /var/run/reboot-required ]; then
+  print_msg "Reboot required. Rebooting now..."
+  reboot
+else
+  print_msg "System update completed successfully. No reboot required."
+fi
+
+# End of the script
+print_msg "Script finished."
